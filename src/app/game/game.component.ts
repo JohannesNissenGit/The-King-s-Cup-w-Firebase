f899f7b00c92b44a6e3c82cb1129b82225e79861
JohannesNissenGit <nissenjohannes@web.de>
2021-04-13 19:46:59 +0200

    firebase download onchange added

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Game } from 'src/models/game';
 import {MatDialog} from '@angular/material/dialog'; //, MatDialogRef, MAT_DIALOG_DATA
 import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-game',
@@ -14,11 +15,19 @@ export class GameComponent implements OnInit {
   currentCard: string = '';
   game!: Game;
   
-  constructor(public dialog: MatDialog) {}
+  constructor(private firestore: AngularFirestore ,public dialog: MatDialog) {}
 
   ngOnInit(): void {
+
     this.newGame();
-    console.log(this.game);
+    this            //firestore valuechanges implemented
+      .firestore
+      .collection('games')
+      .valueChanges()
+      .subscribe((game) => {
+        console.log('Game update: ', game);
+      });
+    
   }
 
   //create new game
