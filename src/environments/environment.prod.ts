f0b62caa8f59ea44109c64fc5915ca2e2132dfe6
JohannesNissenGit <nissenjohannes@web.de>
2021-04-15 11:49:15 +0200

    production environment updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyD4xrR4YyE7IzvMGp4ajfa9jazDJgBx5-Q",
+    authDomain: "kings-72466.firebaseapp.com",
+    projectId: "kings-72466",
+    storageBucket: "kings-72466.appspot.com",
+    messagingSenderId: "1050573994695",
+    appId: "1:1050573994695:web:001f7264562924d42b9ce2"
+  }
 };
